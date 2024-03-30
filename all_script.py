4c5b5b9635a393ec89125480333750c7892b8ae5
Agung Wicaksono <agungwck99@gmail.com>
2024-03-30 08:48:13 +0700

    feat: add write to csv

--- all_script.py
+++ all_script.py
@@ -1,7 +1,9 @@
-from binance_script import get_balance as binance_balance
+from binance_script import get_balance as binance_balance, client
 from gate_script import balance as gate_balance
 from wallet_script import balance_usdt as wallet_balance
 import os
+import pandas as pd
+from datetime import datetime
 
 total_binance, usdt_idr_rate = binance_balance()
 total_gate = gate_balance
@@ -24,3 +26,30 @@ total_usdt = total_binance + total_gate + wallet_balance
 total_idr = total_usdt * usdt_idr_rate
 print(f"Total Asset in USDT: {format(total_usdt, ',.0f')}")
 print(f"Total Asset in IDR: {format(total_idr, ',.0f')}")
+
+print("\n=== Total + Bitget ===")
+manta_bitget = 325
+manta_price = client.ticker_price("MANTAUSDT")
+total_bitget = manta_bitget * float(manta_price['price'])
+total_usdt_with_bitget = total_usdt + total_bitget
+total_idr_with_bitget = total_usdt_with_bitget * usdt_idr_rate
+print(f"Total Asset in USDT: {format(total_usdt_with_bitget, ',.0f')}")
+print(f"Total Asset in IDR: {format(total_idr_with_bitget, ',.0f')}")
+
+
+# Check if the file exists
+file_exists = os.path.isfile('balances.csv')
+
+df = pd.DataFrame({
+    'Date': [datetime.now()],
+    'Binance_USDT': [total_binance],
+    'Gate_USDT': [total_gate],
+    'Wallet_USDT': [wallet_balance],
+    'Total_USDT': [total_usdt],
+    'Total_IDR': [total_idr],
+    'Total_with_other_usdt': [total_usdt_with_bitget],
+    'Total_with_other_idr': [total_idr_with_bitget]
+})
+
+# Append the DataFrame to the CSV file
+df.to_csv('balances.csv', mode='a', header=not file_exists, index=False)
