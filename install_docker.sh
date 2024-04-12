fc2823a94954043e188931ae8499de11952e6192
Agung Wicaksono <agungwck99@gmail.com>
2024-04-12 12:40:03 +0700

    feat: add -y to install docker command

--- install_docker.sh
+++ install_docker.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install docker-ce
-sudo systemctl status docker
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo systemctl status docker --no-pager
 sudo groupadd docker
 sudo usermod -aG docker $USER
 echo "Docker installed successfully"
