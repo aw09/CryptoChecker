350cd0c4f141a2d7c45e081cf20f1f25b5b9c690
Agung Wicaksono <agungwck99@gmail.com>
2024-04-12 12:43:48 +0700

    feat: update install docker

--- install_docker.sh
+++ install_docker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo systemctl status docker --no-pager
-sudo groupadd docker
-sudo usermod -aG docker $USER
-echo "Docker installed successfully"
+sudo apt-get update -y && \
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common && \
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y && \
+sudo apt-get update -y && \
+sudo apt-get install -y docker-ce && \
+sudo systemctl status docker --no-pager && \
+sudo groupadd docker && \
+sudo usermod -aG docker $USER && \
+echo "Docker installed successfully" 
