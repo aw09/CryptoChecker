2fc5a38ab3eed62e2fa1720d66de970626162473
Agung Wicaksono <agungwck99@gmail.com>
2024-04-15 11:23:10 +0700

    feat: change interval

--- telegram_bot.py
+++ telegram_bot.py
@@ -16,8 +16,7 @@ import operator as op
 
 filename = 'balance_vs_btc.csv'
 chartname = 'chart.png'
-refresh_time = 15 * 60 # 15 minutes
-refresh_time_alerts = 5 * 60 # 5 minutes
+refresh_time = 3 * 60 # 15 minutes
 wallet_balance = 600
 
 def read_whitelist():
@@ -302,7 +301,7 @@ def main():
     app.add_handler(CommandHandler("list_alerts", list_alerts))
 
     app.job_queue.run_repeating(updateData, interval=refresh_time, first=0)
-    app.job_queue.run_repeating(check_alerts, interval=refresh_time_alerts, first=0)
+    app.job_queue.run_repeating(check_alerts, interval=refresh_time, first=0)
     app.run_polling()
 
 if __name__ == '__main__':
